b9b171cd3a91882f8c1732b7bd2446379b8bdcaf
Maureen E <maureenekeugo1@gmail.com>
2024-02-17 23:55:10 -0500

    set useState

--- client/src/components/RegisterPage.js
+++ client/src/components/RegisterPage.js
@@ -1,15 +1,48 @@
 import React from 'react'
+import { useState } from 'react';
 
 const RegisterPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handlerName =(env)=>{
+        setUsername(env.target.value)
+    }
+    const handlerPw =(env)=>{
+        setPassword(env.target.value)
+    }
+
+const register =async (ev)=>{
+    ev.preventDefault();
+  const response = await fetch('http://localhost:4000/register',{
+        method : 'POST',
+        body: JSON.stringify({username,password}),
+        headers:{'Content-Type':'application/json'},
+    })
+  console.log(response)
+    if(response.status === 200){
+      alert('Username successful ')
+    }else{
+      alert('Username is unavaible ')
+
+    }
+
+
+}
   return (
     
-    <form className="Register">
+    <form className="Register"onSubmit={register}>
     <h1>Register</h1>
     <input type="text" 
-    placeholder='username'/>
+    placeholder='username'
+     value={username} 
+     onChange={handlerName}/>
     <input type="text" 
-    placeholder='password'/>
+    placeholder='password'
+    value={password}
+   onChange={handlerPw}/>
     <button>Register</button>
+   
 
     </form>
   )
