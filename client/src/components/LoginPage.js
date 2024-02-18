f3977c4773d219cda59175946459932f587fb093
Maureen E <maureenekeugo1@gmail.com>
2024-02-17 23:54:44 -0500

    useState

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const LoginPage = () => {
+
+  const [username,setUsername] = useState('')
+const [password,setPassword] = useState('')
+
+const handlerName =(env)=>{
+  setUsername(env.target.value)
+}
+const handlerPw =(env)=>{
+  setPassword(env.target.value)
+}
+
+
+  const login =async (ev)=>{
+    ev.preventDefault()
+    await  fetch('http://localhost:4000/login',{
+      method : 'POST',
+      body: JSON.stringify({username,password}),
+      headers:{'Content-Type':'application/json'}
+     })
+    }
   return (
    
-        <form className='Login'>
+        <form className='Login' onSubmit={login}>
          <h1>Login</h1>
-        <input type="text" placeholder='username'/>
-        <input type="text" placeholder='password'/>
+        <input type="text" 
+        placeholder='username' 
+        value = {username} 
+        onChange={handlerName}/>
+        <input type="password" 
+        placeholder='password'
+         value={password} 
+         onChange={handlerPw}/>
         <button>Login</button>
 
         </form>
