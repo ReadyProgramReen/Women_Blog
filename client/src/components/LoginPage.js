ddb269db0758fd5ef312a173e800f4bddd87e082
Maureen E <maureenekeugo1@gmail.com>
2024-02-19 13:48:28 -0500

    authorized user login

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
+import {Navigate} from 'react-router-dom';
 
 
 const LoginPage = () => {
 
   const [username,setUsername] = useState('')
 const [password,setPassword] = useState('')
+const [redirect,setRedirect] = useState(false)
 
 const handlerName =(env)=>{
   setUsername(env.target.value)
@@ -17,12 +18,24 @@ const handlerPw =(env)=>{
 
   const login =async (ev)=>{
     ev.preventDefault()
-    await  fetch('http://localhost:4000/login',{
+    const res = await  fetch('http://localhost:4000/login',{
       method : 'POST',
       body: JSON.stringify({username,password}),
-      headers:{'Content-Type':'application/json'}
+      headers:{'Content-Type':'application/json'},
+      credentials:'include',
      })
+     if(res.ok){
+      setRedirect(true)
+    }else{
+      
+      alert('Wrong user name or password ')
+
     }
+    }
+    if(redirect){
+      return <Navigate to={'/'} />
+    }
+
   return (
    
         <form className='Login' onSubmit={login}>
