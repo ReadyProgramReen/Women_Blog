cbd3da6f0fe6c691dffea720aab4e35c5674212d
Maureen E <maureenekeugo1@gmail.com>
2024-02-19 13:47:28 -0500

    restructed nav based on user auth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,49 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom'
+import {useEffect} from 'react';
+import { useState } from 'react';
+
+
+
 
 const Header = () => {
+const [username , setUsername] = useState(null)
+  useEffect(()=>{
+     fetch('http://localhost:4000/profile',{
+      credentials: 'include',
+    }).then(res=>{
+      res.json().then(userInfo =>{
+          setUsername(userInfo.username)
+      })
+    })
+  }, []);
+
+const logout =()=>{
+  fetch('http://localhost:4000/logout',{
+    credentials:'include',
+    method:'POST',
+  });
+  setUsername(null);
+}
+  
   return (
+    
     <header>
     <Link to="/" className="logo">Women In Tech</Link>
     <nav>
+      {username && (
+        <>
+        <Link to="/create">Create new post</Link>
+        <a onClick={logout}>Logout</a>
+        </>
+      )}
+      {!username && (
+        <>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
+        </>
+      )}
+      
     </nav>
   </header>
   )
