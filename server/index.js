647a3f0acb42cbf39e6c5e1748d1b924990adc67
Maureen E <maureenekeugo1@gmail.com>
2024-02-19 13:45:46 -0500

    added route for logout and profile

--- server/index.js
+++ server/index.js
@@ -4,21 +4,25 @@ const cors = require('cors')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 const User = require('./models/User')
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const bcrypt = require('bcryptjs')
 dotenv.config({path: './config/config.env'})
 
 //hash password
 const salt = bcrypt.genSaltSync(10)
+const secret = 'kgfvhjkoiuyfvbgyhjidkefwsftgvhvcvtdtwyvhfuiwdadjcscb '
+
 connectDB()
 
 const app = express()
 
-app.use(cors());
-
+//cors 
+app.use(cors({credentials:true, origin:'http://localhost:3000' }));
 //body parser
-
 app.use(express.json())
+//cookie parser
+app.use(cookieParser())
 
 app.post('/register',async(req,res)=>{
     try {
@@ -35,27 +39,36 @@ app.post('/register',async(req,res)=>{
 })
 
 app.post('/login',async(req,res)=>{
-    // try {
-        const {username,password} = req.body
+
+        const {username,password} = req.body;
     const userDoc = await User.findOne({username})
-    const passOk = bcrypt.compareSync(password, userDoc.password)
-    //     res.json(passOk)
-    // } catch (error) {
-    //     console.error(error, "Passsword or username invalid ")
-
-    // }
+   const passOk =  bcrypt.compareSync(password, userDoc.password)
     if(passOk){
-        res.json('login  successful')
-        
+        //logged in
+        jwt.sign({username,id:userDoc._id}, secret, {},(err,token)=>{
+            if(err)throw err;
+            res.cookie('token', token).json('ok')
+        })
     }else{
-        res.status(400).json('Wrong credentials');
+        res.status(400).json('Wrong info')
     }
-    
-
-    
     
 })
 
+
+app.get('/profile',(req,res)=>{
+ const {token}= req.cookies;
+jwt.verify(token, secret , {},(err,info)=>{
+if(err) throw err;
+res.json(info) 
+
+})
+})
+
+
+app.post('/logout',(req,res)=>{
+    res.cookie('token', '').json('ok');
+})
 
 const PORT = process.env.PORT || 8501
 app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} node on port ${PORT}` ))
