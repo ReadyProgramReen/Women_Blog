355e9532884eabdfa75def0905fd6b9a29395ee0
Maureen E <maureenekeugo1@gmail.com>
2024-02-19 21:01:18 -0500

    added additional  routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,23 @@ import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import { UserContextProvider } from './components/UserContext';
+import CreatePost from './components/CreatePost';
 
 function App() {
   return (
+    <UserContextProvider>
     <Routes>
     <Route path="/" element={<Layout/>}>
     <Route index element= {  <IndexPage/> }/>
    <Route path='/login' element ={ <LoginPage/> }/>
    <Route path='/register'element ={ <RegisterPage/> }/>
-
-
+   <Route path='/create'element ={ <CreatePost/> }/>
+    
     </Route>
-  
-  </Routes>
+   </Routes>
+    </UserContextProvider>
+
   );
 }
 
