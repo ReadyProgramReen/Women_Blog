a3d66b23f09d6c7f62532a4e9876275ae9dcc67d
Maureen E <maureenekeugo1@gmail.com>
2024-02-20 19:24:46 -0500

    made the post data dynamic

--- client/src/components/OtherPost.js
+++ client/src/components/OtherPost.js
@@ -1,18 +1,19 @@
 import React from 'react'
+import {format} from 'date-fns'
 
-const OtherPost = () => {
+const OtherPost = ({title,summary,cover,content,createdAt,author}) => {
   return (
     <div className="post">
           <div className="image">
           <img src="images\image1.jpg" alt="" />
           </div>
         <div className="text">
-          <h2>Top 100 Women in Fashion Tech</h2>
+          <h2>{title}</h2>
           <p className="info">
-            <a href="#" className="author">Maureen E.</a>
-            <time>2023-01-06 16:45</time>
+            <a href="#" className="author">{author.username}</a>
+            <time>{format(new Date(createdAt), "MMM d, yyyy HH:mm")}</time>
           </p>
-          <p className="summary">In 2017, we released our first Top 100 Women in Wearable and Consumer Tech list, in May this year we published our Top 100 Women in Fem Tech and Health Tech list, and now we have compiled another list of top 100 women, this time focusing on top female leaders in fashion tech globally. </p>
+          <p className="summary">{summary}</p>
         </div>
          
         </div>
